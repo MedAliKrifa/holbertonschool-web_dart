add7a60b90955a548ba240c07e7c2880038000c5
MedAliKrifa <daliolimona@gmail.com>
2023-12-06 18:39:43 +0100

    6-inheritance.dart

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,32 +1,34 @@
 class Password {
-  String _password;
+  String _password = "";
 
-  Password({
-    required String password,
-  }) : _password = password;
-
+  Password({required String password}) : _password = password;
+  
   String get password => _password;
-
-  set password(String newPassword) {
-    _password = newPassword;
-  }
+  set password(String value) => _password = value;
 
   bool isValid() {
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
+    bool hasUpperCase = false;
+    bool hasLowerCase = false;
+    bool hasNumber = false;
+
+    for (int i = 0; i < _password.length; i++) {
+      if (_password[i].toUpperCase() == _password[i]) {
+        hasUpperCase = true;
+      }
+      if (_password[i].toLowerCase() == _password[i]) {
+        hasLowerCase = true;
+      }
+      if (int.tryParse(_password[i]) != null) {
+        hasNumber = true;
+      }
     }
 
-    if (!_password.contains(RegExp(r'[A-Z]')) || !_password.contains(RegExp(r'[a-z]'))) {
-      return false;
-    }
-
-    if (!_password.contains(RegExp(r'[0-9]'))) {
-      return false;
-    }
-
-    return true;
+    return _password.length >= 8 &&
+        _password.length <= 16 &&
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumber;
   }
-
   @override
   String toString() {
     return "Your Password is: $_password";
